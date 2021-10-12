62e615db53dfcd1f230b4a5d2b8831fc9adb8d4a
Shivey Singh <shivey_khangura@hotmail.com>
2021-10-12 17:02:50 +0100

    Added function, loop and if statement

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,3 +1,24 @@
 #!/usr/bin/env bash 
 # File: guessinggame.sh
 # Peer graded assignment for Coursera The Unix Workbench
+ 
+function guessinggame {
+  while [[ $guess -ne $files ]]
+  do 
+    if [[ $guess -lt $files ]]
+    then
+      echo "Your guess is too low. Please guess again:"
+      read guess
+    elif [[ $guess -gt files ]]
+    then
+      echo "Your guess is too high. Please guess again:"
+      read guess
+    fi
+  done
+  echo "Congratulations! Your answer is correct!"
+}
+
+echo "Plese enter how many files are in the current directory:"
+read guess
+files=$(ls | wc -l)
+guessinggame guess files
